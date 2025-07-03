fc075c7ca95f8e12f4d1773b9cd38e0c73afbe8b
Shree Harinesh S <shreeharineshs@gmail.com>
2025-07-03 20:10:32 +0530

    Implemented the Spring Core Exercise 2: Implementing Dependency Injection

---
+++ src/Week3/Spring_Core_Maven/Ex2ImplementingDependencyInjection/Ex2LibraryManagement/Ex2LibraryManagement/src/main/java/com/example/Ex2LibraryManagement/Service/BookService.java
@@ -0,0 +1,20 @@
+package com.example.Ex2LibraryManagement.Service;
+
+import com.example.Ex2LibraryManagement.Repository.BookRepository;
+
+public class BookService
+{
+    private BookRepository bookRepository;
+
+    // Setter for DI
+    public void setBookRepository(BookRepository bookRepository)
+    {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName)
+    {
+        System.out.println("BookService: Adding book...");
+        bookRepository.saveBook(bookName);
+    }
+}
---
+++ src/Week3/Spring_Core_Maven/Ex2ImplementingDependencyInjection/Ex2LibraryManagement/Ex2LibraryManagement/src/test/java/com/example/Ex2LibraryManagement/Ex2LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Week3.Spring_Core_Maven.Ex2ImplementingDependencyInjection.Ex2LibraryManagement.Ex2LibraryManagement.src.test.java.com.example.Ex2LibraryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
