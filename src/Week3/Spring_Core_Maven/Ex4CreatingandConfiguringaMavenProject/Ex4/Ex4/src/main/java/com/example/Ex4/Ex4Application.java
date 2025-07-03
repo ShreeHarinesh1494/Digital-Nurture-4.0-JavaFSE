6ef4bfa458caba2bd0fe3b0feb9e3619eace8294
Shree Harinesh S <shreeharineshs@gmail.com>
2025-07-03 20:25:27 +0530

    Implemented the Spring Core Exercise 4: Creating and Configuring a Maven Project

---
+++ src/Week3/Spring_Core_Maven/Ex4CreatingandConfiguringaMavenProject/Ex4/Ex4/src/main/java/com/example/Ex4/Ex4Application.java
@@ -0,0 +1,13 @@
+package Week3.Spring_Core_Maven.Ex4CreatingandConfiguringaMavenProject.Ex4.Ex4.src.main.java.com.example.Ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex4Application.class, args);
+	}
+
+}
---
+++ src/Week3/Spring_Core_Maven/Ex4CreatingandConfiguringaMavenProject/Ex4/Ex4/src/test/java/com/example/Ex4/Ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package Week3.Spring_Core_Maven.Ex4CreatingandConfiguringaMavenProject.Ex4.Ex4.src.test.java.com.example.Ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
